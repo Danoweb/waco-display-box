371f12c81a9634e08310ce337b36a0a79eea0130
Danoweb <danreed1986@gmail.com>
2024-08-29 21:03:55 +0000

    updated(slide): added init images and slideshow capability

--- src/src/App.js
+++ src/src/App.js
@@ -1,22 +1,24 @@
+import React from 'react';
 import './App.css';
 
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
-import { loadImages } from 'src/components/loadImages'
+import ImageGallery from './ImageGallery';
 
 
 function App() {
-  const imagePaths = loadImages(`${process.env.REACT_APP_IMAGE_FOLDER}`)
+  // const imagePaths = loadImages(`${process.env.REACT_APP_IMAGE_FOLDER}`)
   
   return (
-    <div className="App">
-      <Slide>
-        {imagePaths.map((path, index) => (
-            <div className="each-slide" key={index}>
-                <img src={path} alt={`Slide ${index}`} style={{ width: '100%'}} />
-            </div>
-        ))}
-      </Slide>
+    // <div className="App">
+    //   <Slide>
+    //     {imagePaths.map((path, index) => (
+    //         <div className="each-slide" key={index}>
+    //             <img src={path} alt={`Slide ${index}`} style={{ width: '100%'}} />
+    //         </div>
+    //     ))}
+    //   </Slide>
+    // </div>
+    <div>
+      <ImageGallery />
     </div>
   );
 }
---
+++ src/src/ImageGallery.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+const ImageGallery = () => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        // Assuming images are stored in the public/images folder
+        const importAll = (r) => r.keys().map(r);
+        const images = importAll(require.context(process.env.REACT_APP_IMAGE_FOLDER, false, /\.(png|jpe?g|svg)$/));
+
+        setImages(images);
+    }, []);
+
+    return (
+        <div className="slide-container">
+          <Slide easing="ease" autoplay={true} duration={5000}>
+            {images.map((image, index) => (
+              <div className="each-slide" key={index}>
+                <img
+                    key={index}
+                    src={process.env.PUBLIC_URL + image}
+                    alt={`Gallery ${index}`}
+                    style={{ width: process.env.REACT_APP_IMAGE_WIDTH, maxWidth: '100%', height: 'auto' }}
+                />
+              </div>
+            ))}
+          </Slide>
+        </div>
+    );
+};
+
+export default ImageGallery;
